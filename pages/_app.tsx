d33b12294deb6ccb8bb946df58f999000a36ee95
Typhlosion123 <chrisyxu7@gmail.com>
2025-04-07 10:54:55 -0500

    fonts please work man

---
+++ pages/_app.tsx
@@ -0,0 +1,19 @@
+import type { AppProps } from 'next/app';
+import Head from 'next/head';
+import '../styles/globals.css';
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Head>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Ubuntu:wght@300;400;500;700&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
+}
+
+export default MyApp;
