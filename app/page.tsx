488553245b003c0e675446edc0934b054d2c2cef
Typhlosion123 <chrisyxu7@gmail.com>
2025-04-07 02:29:49 -0500

    oh lord please

--- app/page.tsx
+++ app/page.tsx
@@ -1,37 +1,43 @@
 'use client';
 
 import { useState, useEffect } from "react";
+import Spline from '@splinetool/react-spline';
 
 export default function Home() {
   const [activeModel, setActiveModel] = useState(1);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     const timer = setTimeout(() => {
-      setActiveModel(2); // After 7 seconds, switch to the second model
+      setActiveModel(2); // After 9 seconds, switch to the second model
     }, 9000);
 
-    return () => clearTimeout(timer); // Clean up the timer when the component unmounts
+    return () => clearTimeout(timer);
   }, []);
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-0 bg-black w-screen h-screen">
       <div className="w-full h-full">
         {activeModel === 1 ? (
-          <iframe
-            src="https://my.spline.design/progressbarscrollevent-3aac3574d664080593953458a814650d/"
-            frameBorder="0"
+          <Spline
+            scene="https://prod.spline.design/7JTroMEqlN2ok6WN/scene.splinecode"
             className="w-full h-full"
-            allow="autoplay; fullscreen"
-            onLoad={() => console.log('Model 1 Loaded')}
-          ></iframe>
+            onLoad={() => {
+              console.log('Model 1 Loaded');
+              setIsLoading(false);
+            }}
+            onError={() => console.error('Error loading Model 1')}
+          />
         ) : (
-          <iframe
-            src="https://my.spline.design/thepc-de0c3202fab3683d455ead2170633d43/"
-            frameBorder="0"
+          <Spline
+            scene="https://prod.spline.design/6UTcgkxAaa7nu36T/scene.splinecode"
             className="w-full h-full"
-            allow="autoplay; fullscreen"
-            onLoad={() => console.log('Model 2 Loaded')}
-          ></iframe>
+            onLoad={() => {
+              console.log('Model 2 Loaded');
+              setIsLoading(false);
+            }}
+            onError={() => console.error('Error loading Model 2')}
+          />
         )}
       </div>
     </main>
