32e28fd0abfc86b7d8d610b527e6471a3731a49e
Typhlosion123 <chrisyxu7@gmail.com>
2025-04-06 21:41:29 -0500

    full screen v3

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,9 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-0 bg-black">
       <h1 className="text-3xl font-bold mb-4 text-white">Spline 3D Model</h1>
-      <div className="w-full h-full">
+      <div className="w-[2500] h-[1100]">
         <iframe
-          src="https://prod.spline.design/6UTcgkxAaa7nu36T/scene.splinecode"
+          src="https://my.spline.design/thepc-de0c3202fab3683d455ead2170633d43/"
           frameBorder="0"
           width="100%"
           height="100%"
