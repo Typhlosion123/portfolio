299de342b16c1f8b939eedabe8d7067660547747
Typhlosion123 <chrisyxu7@gmail.com>
2025-04-07 11:04:54 -0500

    few changes

--- app/aboutme/page.tsx
+++ app/aboutme/page.tsx
@@ -16,7 +16,7 @@ export default function AboutMe() {
         <div className="absolute left-1/2 transform -translate-x-[40%] w-px h-full bg-white" />
 
         {/* Left Side: Name and Contact */}
-        <div className="w-1/2 pr-12 flex flex-col justify-center items-end text-right font-ubuntu">
+        <div className="w-1/2 pr-12 flex flex-col justify-center items-end text-right">
           <h1 className="text-4xl font-bold mb-4">Chris Xu</h1>
           <p className="text-lg">Email: cxu57@illinois.edu</p>
           <div className="mt-4 space-y-100">
@@ -40,7 +40,7 @@ export default function AboutMe() {
         </div>
 
         {/* Right Side: Information */}
-        <div className="w-1/2 pl-12 flex flex-col justify-center font-ubuntu">
+        <div className="w-1/2 pl-12 flex flex-col justify-center">
           <h2 className="text-2xl font-semibold mb-2">About Me</h2>
           <p className="text-base leading-relaxed">
             I’m a passionate developer with a love for solving complex problems and building interactive web applications. I enjoy working with modern technologies and always seek to learn something new.
