7df72f4d0b8f50e7df33a38a796c7a3ecb8507c3
Typhlosion123 <chrisyxu7@gmail.com>
2025-04-07 02:43:53 -0500

    adding aboutme folder and shii

---
+++ app/aboutme/page.tsx
@@ -0,0 +1,19 @@
+'use client';
+
+export default function About() {
+  return (
+    <main className="relative w-screen h-screen bg-black overflow-hidden">
+      <iframe
+        src="https://my.spline.design/patternbackground-8e7c0e03665c5a8c13b95812f0fc8909/" // Replace with your actual model URL
+        frameBorder="0"
+        className="w-full h-full"
+        allow="autoplay; fullscreen"
+      ></iframe>
+
+      {/* Subbar */}
+      <div className="absolute bottom-0 w-full text-center text-white text-sm py-2 bg-black/50 backdrop-blur-sm">
+        © 2025 Chris Xu. All rights reserved.
+      </div>
+    </main>
+  );
+}
