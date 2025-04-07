1436562d8d42cadf7b5db778d2a6dfba0e89e458
Typhlosion123 <chrisyxu7@gmail.com>
2025-04-07 10:58:03 -0500

    alsjflasjf

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
-import 'app/globals.css';
+import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
