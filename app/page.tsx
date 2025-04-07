18029ddd794d8a7c43c507f0724daf269e4a0568
Typhlosion123 <chrisyxu7@gmail.com>
2025-04-06 23:23:42 -0500

    scrren sizing v2

--- app/page.tsx
+++ app/page.tsx
@@ -14,23 +14,21 @@ export default function Home() {
   }, []);
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center p-0 bg-black">
-      <h1 className="text-3xl font-bold mb-4 text-white">Spline 3D Model</h1>
+    <main className="flex min-h-screen flex-col items-center justify-center p-0 bg-black w-screen h-screen">
+      <h1 className="text-3xl font-bold mb-4 text-white absolute z-10">Spline 3D Model</h1>
       <div className="w-2500 h-1100">
         {activeModel === 1 ? (
           <iframe
             src="https://my.spline.design/progressbarscrollevent-3aac3574d664080593953458a814650d/"
             frameBorder="0"
-            width="100%"
-            height="100%"
+            className="w-full h-full"
             allow="autoplay; fullscreen"
           ></iframe>
         ) : (
           <iframe
             src="https://my.spline.design/thepc-de0c3202fab3683d455ead2170633d43/"
             frameBorder="0"
-            width="100%"
-            height="100%"
+            className="w-full h-full"
             allow="autoplay; fullscreen"
           ></iframe>
         )}
