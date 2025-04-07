5cfb3750bb3ecababfd1e52ea5d01c502a0e051d
Typhlosion123 <chrisyxu7@gmail.com>
2025-04-06 21:39:38 -0500

    full screen v2

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
       <h1 className="text-3xl font-bold mb-4 text-white">Spline 3D Model</h1>
       <div className="w-full h-full">
         <iframe
-          src="https://my.spline.design/thepc-de0c3202fab3683d455ead2170633d43/"
+          src="https://prod.spline.design/6UTcgkxAaa7nu36T/scene.splinecode"
           frameBorder="0"
           width="100%"
           height="100%"
